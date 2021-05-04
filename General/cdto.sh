4a535e0260cbd884726331e59ca295c5bab4d7da
oopsmails <oopsmails@gmail.com>
2021-05-03 20:57:22 -0400

    20210503: cdto

--- General/cdto.sh
+++ General/cdto.sh
@@ -1,31 +1,70 @@
 #echo $1
 
 
-#### /c/GitHub
+#### define path locally
+
+github="/c/sharing/github"
+githubother="/c/sharing/github-other"
 
 ##
 if test "$1" = "github"
 then
-	cd /c/GitHub
+	cd $github
+##
+
+##
+elif test "$1" = "githubother"
+then
+	cd $githubother
 ##
 
 ##
 elif test "$1" = "generalinfo"
 then
-	cd /c/github/general-info/
+	cd $github/general-info/
 ##
 
-
 ##
-elif test "$1" = "githubother"
+elif test "$1" = "sharing"
 then
-	cd /c/githubother/skeleton
+	cd /c/sharing/
 ##
 
+
+## ------------------------ special cases ----------------------------
+
+##
+elif test "$1" = "usingwait"
+then
+	cd /c/sharing/
+	wait
+	pwd
+##
+
+##
+elif test "$1" = "keeplivepy"
+then
+	cd /home/albert/Documents/programs
+	python3 keepLiveByMouseMoving.py
+##
+
+##
+elif test "$1" = "keeplive"
+then
+	# cd /c/sharing/
+	java -cp /c/sharing MainMouseEvents
+##
+
+##
+elif test "$1" = "nosqlbooster"
+then
+	cd /home/albert/Documents/programs
+	./nosqlbooster4mongo-6.2.8.AppImage &
+##
 
 
 ##
 else
-	echo "Path Not Defined in /c\Liu\cdto.sh"
+	echo "Path Not Defined in /c\sharing\cdto.sh"
 fi
 ##
